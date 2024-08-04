41deff39b5f8110e011f83b43ddcc4f9a4c5aa82
wl-eeee <129074330+wl-eeee@users.noreply.github.com>
2024-08-04 10:57:56 +0800

    Update 链表.c

--- 初始化操作/链表.c
+++ 初始化操作/链表.c
@@ -8,24 +8,41 @@ typedef struct node {
 
 Lnode* init()
 {
-	Lnode* head = (Lnode*)malloc(sizeof(Lnode));
-	head->data = 0;
-	head->next = NULL;
+	Lnode* Lhead = (Lnode*)malloc(sizeof(Lnode));
+	Lhead->data = -1;
+	Lhead->next = NULL;
 
 
-	return head;
+	return Lhead;
 }
 
-void insertbefore(Lnode* head)
+void insertbefore(Lnode* Lhead)
 {
 	int n = 0;
 	for (; n < 10; n++)
 	{
 		Lnode* L = (Lnode*)malloc(sizeof(Lnode));
 		L->data = 10 - n;
-		L->next = head->next;
-		head->next = L;
+		L->next = Lhead->next;
+		Lhead->next = L;
 	}
+}
+
+Lnode* function(Lnode* Lhead)
+{
+
+	Lnode* Qhead = (Lnode*)malloc(sizeof(Lnode));
+	Qhead->data = -1;
+	Qhead->next = NULL;
+
+	Lnode* Q = (Lnode*)malloc(sizeof(Lnode));
+	Q->data = 0;
+	Q->next = NULL;
+	Qhead->next = Q;//Q 遍历用    Qin 插入用
+
+
+
+	return Qhead;
 }
 
 
@@ -33,11 +50,11 @@ void insertbefore(Lnode* head)
 
 int main()
 {
-	Lnode* head = init();
-	insertbefore(head);
+	Lnode* Lhead = init();
+	insertbefore(Lhead);
 
 	int n = 0;
-	Lnode* L = head->next;
+	Lnode* L = Lhead->next;
 	puts("input sequence");
 	while (L->next != NULL)
 	{
@@ -49,15 +66,29 @@ int main()
 
 
 
-	L = head->next;
+	
+
+	L = function(Lhead);
+	L = L->next;
 	puts("output sequence");
-	while (L->next != NULL)
+	while (L != NULL)
 	{
 		printf("%d ", L->data);
 		L = L->next;
 	}
-	printf("%d ", L->data);
-
+	
 
 	return 0;
 }
+
+//如果需要链表是乱序 使用这个插入
+/*
+Lhead->next->data = 4;
+	Lhead->next->next->data = 3;
+	Lhead->next->next->next->data = 2;
+	Lhead->next->next->next->next->data = 9;
+	Lhead->next->next->next->next->next->data = 5;
+	Lhead->next->next->next->next->next->next->data = 2;
+	Lhead->next->next->next->next->next->next->next->data = 1;
+	Lhead->next->next->next->next->next->next->next->next->data = 8;
+	Lhead->next->next->next->next->next->next->next->next->next->data = 7;*/
